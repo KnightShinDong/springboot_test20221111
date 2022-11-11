4c056fe44a026fd93b9112577ba8bcc7efcf1f26
ici <ici@DESKTOP-8CT0OFV>
2022-11-11 16:01:49 +0900

    스프링부트 테스트

---
+++ src/main/java/com/gyojincompany/test1/Test2Controller.java
@@ -0,0 +1,17 @@
+package com.gyojincompany.test1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Test2Controller {
+
+	@RequestMapping("test")
+	@ResponseBody
+	public String test() {
+		
+		return "안녕하세요 반갑습니다.";
+	}
+	
+}
